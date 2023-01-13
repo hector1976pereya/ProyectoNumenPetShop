e67b2b8e77f631e5a617797a67e22af809c85a3a
hector pereyra <arnahect@gmail.com>
2023-01-13 16:36:21 -0300

    Se agrega barra de navegacion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
+import React from 'react';
+import Navbar from './components/Navbar';
 import './App.css';
-import NavBarr from './components/navbar/NavBarr';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Productos from './components/pages/Productos';
+import Servicios from './components/pages/Servicios';
+import Contacto from './components/pages/Contacto';
+import Carrito from './components/pages/Carrito';
+import Perros from './components/pages/Productos-perros';
+import Gatos from './components/pages/Productos-gatos';
+import Indumentaria from './components/pages/Indumentaria';
+import Accesorios from './components/pages/Accesorios';
 
 function App() {
-  return (<>
-    <NavBarr/> 
+  return (
     
-  </>
-    
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/productos" element={<Productos />} />
+          <Route path="/servicios" element={<Servicios />} />
+          <Route path="/contacto" element={<Contacto />} />
+          <Route path="/carrito" element={<Carrito />} />   
+          <Route path="/Productos-perros" element={<Perros />} />
+          <Route path="/Productos-gatos" element={<Gatos />} />   
+          <Route path="/Indumentaria" element={<Indumentaria />} />  
+          <Route path="/Accesorios" element={<Accesorios />} />          
+      </Routes>
+            
+    </Router>
+     
   );
 }
 
